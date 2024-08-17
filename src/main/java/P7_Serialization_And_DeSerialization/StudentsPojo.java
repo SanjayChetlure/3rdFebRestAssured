1bf338277f65650331fdd5b8a0a9bccc4e596fe0
SanjayChetlure <sanjay.chetlure@gmail.com>
2024-08-17 20:31:47 +0530

    17th Aug 2024

---
+++ src/main/java/P7_Serialization_And_DeSerialization/StudentsPojo.java
@@ -0,0 +1,45 @@
+package P7_Serialization_And_DeSerialization;
+
+import java.util.ArrayList;
+
+public class StudentsPojo 
+{
+		private String id;
+		private String name;
+		private String location;
+		private String phone;
+		private String [] cources;
+		
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getLocation() {
+			return location;
+		}
+		public void setLocation(String location) {
+			this.location = location;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String[] getCources() {
+			return cources;
+		}
+		public void setCources(String[] cources) {
+			this.cources = cources;
+		}
+	
+
+}
